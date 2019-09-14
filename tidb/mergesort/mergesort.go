41d027e2d9f8b84259c67c21818dc39115103036
zhongzc <zhongzc_arch@outlook.com>
2019-09-14 14:52:22 +0800

    mergesort: add some comments

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -11,26 +11,31 @@ func MergeSort(src []int64) {
 	mergeSort(src, chunk{0, len(src)}, make([]int64, len(src)))
 }
 
-var ParallelRate = runtime.NumCPU()
-var InsertThresholds = 1 << 8
 
-type chunk struct {
-	from  int
-	limit int
-}
+var (
+	// ParallelRate -- the number of sub- merge tasks split by a merge task
+	//                 default: the number of cpu cores
+	ParallelRate = runtime.NumCPU()
+	// InsertThresholds -- the merge sort function will turn to insert sort 
+	//                     when unsorted slice's size reaches this boundary
+    InsertThresholds = 1 << 8
+)
 
-func (c chunk) size() int {
-	return c.limit - c.from
-}
-
+/* 
+	sort a slice thunk using merge sort algroithm
+	c   -- the range of slice to be sorted
+	aux -- a auxiliary slice, its size must greater than or equal to src's 
+*/
 func mergeSort(src []int64, c chunk, aux []int64) {
 	if c.size() <= InsertThresholds || c.size() <= ParallelRate {
 		insertSort(src, c)
 		return
 	}
 
-	chunks := sliceChuck(c, ParallelRate)
+	chunks := splitChunks(c, ParallelRate)
 
+	// each chunk of the entire slice will be processing
+	// in a new goroutine
 	var wg sync.WaitGroup
 	wg.Add(len(chunks))
 	for _, c := range chunks {
@@ -41,11 +46,15 @@ func mergeSort(src []int64, c chunk, aux []int64) {
 	}
 	wg.Wait()
 
+	// need to copy a src slice to aux slice, otherwise
+	// the sub-slices to be used would be erased
 	copy(aux[c.from:c.limit], src[c.from:c.limit])
+
 	merge(src, aux, c, chunks)
 }
 
-func sliceChuck(c chunk, n int) []chunk {
+// split a chunk c to n sub-chunks
+func splitChunks(c chunk, n int) []chunk {
 	var res = make([]chunk, 0, n)
 	sz := c.size() / n
 	for i := 0; i < n; i++ {
@@ -58,8 +67,12 @@ func sliceChuck(c chunk, n int) []chunk {
 	return res
 }
 
+/*
+	merge a bunch of sub-slices from aux slice to tgt slice
+	c      -- the sub-slice will be merged in
+	chunks -- sorted sub-slices, will be merged to c
+*/
 func merge(tgt []int64, aux []int64, c chunk, chunks []chunk) {
-
 	for i := c.limit - 1; i >= c.from; i-- {
 		idx := -1
 		for j := 0; j < len(chunks); j++ {
@@ -72,10 +85,11 @@ func merge(tgt []int64, aux []int64, c chunk, chunks []chunk) {
 			}
 		}
 		tgt[i] = aux[chunks[idx].limit-1]
-		chunks[idx].limit -= 1
+		chunks[idx].limit--
 	}
 }
 
+// insert sort is for small size slice
 func insertSort(src []int64, c chunk) {
 	for i := c.from; i < c.limit; i++ {
 		for j := i - 1; j >= c.from; j-- {
@@ -84,4 +98,14 @@ func insertSort(src []int64, c chunk) {
 			}
 		}
 	}
+}
+
+// sub part of the entire slice
+type chunk struct {
+	from  int
+	limit int
+}
+
+func (c chunk) size() int {
+	return c.limit - c.from
 }
